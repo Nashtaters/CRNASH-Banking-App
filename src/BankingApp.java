f4780925924e979c0b4a6986f7572b9431ec6f07
Curtis Nash <curtis.r.nash@gmail.com>
2022-09-30 16:49:20 -0600

    Fixed GUI

--- src/BankingApp.java
+++ src/BankingApp.java
@@ -9,8 +9,11 @@ public class BankingApp extends JFrame implements ActionListener {
     private JButton _deposit = null;
     private JButton _balance = null;
     private JButton _exit = null;
-    private JTextArea _display = null;
-    private JTextField _withdrawInput = null;
+    private JPanel _display = null;
+    private JPanel _widgetPanel = null;
+    private JLabel _prompt = null;
+    private JTextField _input = null;
+    private JButton _submit = null;
     private Account _account = new Account(1000);
 
     public static void main(String[] args) {
@@ -48,10 +51,24 @@ public class BankingApp extends JFrame implements ActionListener {
         _exit.addActionListener(this);
         buttonPanel.add(_exit);
 
-        _display = new JTextArea();
-        _display.setAlignmentX(Component.LEFT_ALIGNMENT);
-        _display.setEditable(false);
-        _display.setText("Hello All");
+        _display = new JPanel();
+        _display.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        _widgetPanel = new JPanel();
+        _widgetPanel.setLayout(new FlowLayout());
+        _display.add(_widgetPanel, gbc);
+
+        _prompt = new JLabel("Enter amount to withdraw: ");
+
+        _input = new JTextField("");
+        _input.setPreferredSize(new Dimension(100, 25));
+        _submit = new JButton("Submit");
+        _widgetPanel.setVisible(false);
+
+        _widgetPanel.add(_prompt);
+        _widgetPanel.add(_input);
+        _widgetPanel.add(_submit);
 
         setLayout(new BorderLayout());
         add(buttonPanel, BorderLayout.WEST);
@@ -59,30 +76,23 @@ public class BankingApp extends JFrame implements ActionListener {
         add(_display, BorderLayout.CENTER);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        // TODO make new text field and hide it until it needs to be displayed
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton)e.getSource();
-
         if (button == _withdraw) {
-            System.out.println("Enter amount to withdraw: ");
-            performWithrawl();
+            performWithdrawal();
         }
-        else if (button == _deposit){
-            System.out.println("Enter amount to deposit: ");
+        else if (button == _deposit) {
         }
         else if (button == _balance){
-            System.out.println("Your current balance is " + _account.getBalance());
         }
         else if (button == _exit) {
             System.exit(0);
         }
     }
-    protected float performWithrawl() {
-        return 0.0f;
+    protected void performWithdrawal() {
+        _widgetPanel.setVisible(true);
     }
 }
